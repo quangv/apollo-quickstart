588e2153c4a87fbbc65914148a394faaa7ae2143
Quang Van <quang@absentsoft.com>
2018-06-03 19:27:23 -0400

    query ExchangeRates

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import registerServiceWorker from './registerServiceWorker';
 import ApolloClient from "apollo-boost";
 import gql from "graphql-tag";
 
-import { ApolloProvider } from "react-apollo";
+import { ApolloProvider, Query } from "react-apollo";
 
 const client = new ApolloClient({
     uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
@@ -27,9 +27,33 @@ client
     .then(result => console.log(result))
 */
 
+const ExchangeRates = () => (
+    <Query
+      query={gql`{
+        rates(currency: "USD") {
+          currency
+          rate
+        }
+      }`}>
+      {({ loading, error, data }) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) return <p>Error :(</p>;
+
+        return data.rates.map(({ currency, rate }) => (
+          <div key={currency}>
+            <p>{`${currency}: ${rate}`}</p>
+          </div>
+        ))
+      }}
+    </Query>
+)
+
 const ApolloApp = () => (
     <ApolloProvider client={client}>
-      <App />
+      <React.Fragment>
+        <App />
+        <ExchangeRates />
+      </React.Fragment>
     </ApolloProvider>
 );
 
