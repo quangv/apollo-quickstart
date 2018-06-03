9534f800953ed9787d8445e360f83e5755db7026
Quang Van <quang@absentsoft.com>
2018-06-03 19:00:14 -0400

    apollo client

--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+    uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+})
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
